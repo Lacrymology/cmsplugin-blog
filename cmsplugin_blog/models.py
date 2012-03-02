6fefea94b493c12d796ba3b9d6450d8db42b2453
Tomas Neme <lacrymology@gmail.com>
2012-03-02 11:47:33 -0300

    pylinted models

--- cmsplugin_blog/models.py
+++ cmsplugin_blog/models.py
@@ -20,7 +20,8 @@ from djangocms_utils.fields import M2MPlaceholderField
 class PublishedEntriesQueryset(QuerySet):
     
     def published(self):
-        return self.filter(is_published=True, pub_date__lte=datetime.datetime.now())
+        return self.filter(is_published=True,
+                           pub_date__lte=datetime.datetime.now())
         
 class EntriesManager(models.Manager):
     
@@ -29,18 +30,22 @@ class EntriesManager(models.Manager):
                             
 class PublishedEntriesManager(EntriesManager):
     """
-        Filters out all unpublished and items with a publication date in the future
+    Filters out all unpublished and items with a publication date in the future
     """
     def get_query_set(self):
         return super(PublishedEntriesManager, self).get_query_set().published()
                     
-CMSPLUGIN_BLOG_PLACEHOLDERS = getattr(settings, 'CMSPLUGIN_BLOG_PLACEHOLDERS', ('excerpt', 'content'))
+CMSPLUGIN_BLOG_PLACEHOLDERS = getattr(settings, 'CMSPLUGIN_BLOG_PLACEHOLDERS',
+                                      ('excerpt', 'content'))
               
 class Entry(models.Model):
     is_published = models.BooleanField(_('is published'))
-    pub_date = models.DateTimeField(_('publish at'), default=datetime.datetime.now)
+    pub_date = models.DateTimeField(_('publish at'),
+                                    default=datetime.datetime.now)
  
-    placeholders = M2MPlaceholderField(actions=SimpleTranslationPlaceholderActions(), placeholders=CMSPLUGIN_BLOG_PLACEHOLDERS)
+    placeholders = M2MPlaceholderField(
+        actions=SimpleTranslationPlaceholderActions(),
+        placeholders=CMSPLUGIN_BLOG_PLACEHOLDERS)
     
     tags = TagField()
     
@@ -68,8 +73,10 @@ class Entry(models.Model):
         blog_prefix = ''
 
         try:
-            title = Title.objects.get(application_urls='BlogApphook', language=language)
-            blog_prefix = urljoin(reverse('pages-root'), title.overwrite_url or title.slug)
+            title = Title.objects.get(application_urls='BlogApphook',
+                                      language=language)
+            blog_prefix = urljoin(reverse('pages-root'),
+                                  title.overwrite_url or title.slug)
         except Title.DoesNotExist:
             # Blog app hook not defined anywhere?
             pass
@@ -94,16 +101,21 @@ class AbstractEntryTitle(models.Model):
     DETAIL_TEMPLATE = 'cmsplugin_blog/entry_detail.html'
     
     entry = models.ForeignKey(Entry, verbose_name=_('entry'))
-    language = models.CharField(_('language'), max_length=15, choices=settings.LANGUAGES)
+    language = models.CharField(_('language'), max_length=15,
+                                choices=settings.LANGUAGES)
     title = models.CharField(_('title'), max_length=255)
     slug = models.SlugField(_('slug'), max_length=255)
-    author = models.ForeignKey('auth.User', null=True, blank=True, verbose_name=_("author"))
+    author = models.ForeignKey('auth.User', null=True, blank=True,
+                               verbose_name=_("author"))
     
     def __unicode__(self):
         return self.title
         
     def _get_absolute_url(self):
-        language_namespace = 'cmsplugin_blog.middleware.MultilingualBlogEntriesMiddleware' in settings.MIDDLEWARE_CLASSES and '%s:' % self.language or ''
+        language_namespace = (('cmsplugin_blog.middleware.'
+                               'MultilingualBlogEntriesMiddleware') in
+                              settings.MIDDLEWARE_CLASSES and
+                              '%s:' % self.language or '')
         return ('%sblog_detail' % language_namespace, (), {
             'year': self.entry.pub_date.strftime('%Y'),
             'month': self.entry.pub_date.strftime('%m'),
@@ -127,6 +139,8 @@ class LatestEntriesPlugin(CMSPlugin):
         Model for the settings when using the latest entries cms plugin
     """
     limit = models.PositiveIntegerField(_('Number of entries items to show'), 
-                    help_text=_('Limits the number of items that will be displayed'))
+                    help_text=_('Limits the number of items that will be '
+                                'displayed'))
                     
-    current_language_only = models.BooleanField(_('Only show entries for the current language'))
+    current_language_only = models.BooleanField(_('Only show entries for the '
+                                                  'current language'))
